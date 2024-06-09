a4cdc9d14c2abad63de81c277a3f0961c5821b56
ElonaHoxhaj96 <elona6915@gmail.com>
2024-06-10 00:35:20 +0200

    Definded Player and Computer player

---
+++ dadi/main.js
@@ -0,0 +1,9 @@
+// facciamo il gioco dei dadi, dobbiamo far lanciare un dado al giocatore/utente e un dado al computer, quello che ha il numero maggiore vince.
+
+//Definiamo il giocatore 
+let player = document.getElementById('giocatore');
+console.log(player)
+
+//Definiamo il computer 
+let computerPlayer = document.getElementById('computer')
+console.log(computerPlayer)
--- mail/main.js
+++ mail/main.js
@@ -2,13 +2,14 @@
 const  mailList = ['massimorossi@gmail.com','teresabianchi@gmail.com','gianpierpaolo30@hotmail.it','gianni68@gmail.com','filippo.giani@outlook.com',' paolo56@gmail.com'];
 
 //chiedi all'utente di inserire la sua mail
-let mailUtente = (parseInt(prompt("Inserisci la tua mail")));
+let inputMail = document.getElementById("mailUtente").value += prompt('Inserisci la tua mail');
+console.log(inputMail)
 
 //verifica che la mail sia presente nella mia lista;
 for(let i = 0; i < mailList.length; i++){
     //se la mail è in lista dai il benvenuto;
     let textmessage;
-    if (mailUtente === mailList){
+    if (inputMail === mailList){
         textmessage = "Benvenuto";
     }else{ // altrimenti nega l'accesso;
        textmessage = "Accesso negato, la tua mail non è presente nel sistema";
