9633aeefd6a6d2aae8d3a6f0adfe29e4994fdc05
ElonaHoxhaj96 <elona6915@gmail.com>
2024-06-10 01:00:43 +0200

    Aded condition to my game

--- dadi/main.js
+++ dadi/main.js
@@ -1,9 +1,22 @@
 // facciamo il gioco dei dadi, dobbiamo far lanciare un dado al giocatore/utente e un dado al computer, quello che ha il numero maggiore vince.
 
 //Definiamo il giocatore 
-let player = document.getElementById('giocatore');
+let player =  Math.floor(Math.random()*7);
 console.log(player)
 
 //Definiamo il computer 
-let computerPlayer = document.getElementById('computer')
-console.log(computerPlayer)
+computerPlayer = Math.floor(Math.random()*7);
+console.log(computerPlayer);
+//nominiamo la variabile del risultato
+const risultato = document.getElementById('result');
+
+//creiamo le condizioni del gioco
+if(player > computerPlayer){
+    risultato.innerHTML += ('Hai vintooo!!')
+}else{
+    risultato.innerHTML += ('Hai perso!!')
+}
+console.log(risultato);
+
+document.getElementById("giocatore").innerHTML= "il tuo numero è "+ player;
+document.getElementById("computer").innerHTML= "il numero del computer è "+ computerPlayer;
